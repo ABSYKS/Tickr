a35680d5e5c55df80eb8df0eb03343c62cb45813
Absyks <besseysimoo@hotmail.com>
2016-05-11 19:47:40 +0100

    Initial Commit

---
+++ TickrApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TickrApp
+//
+//  Created by Simon Bessey on 11/05/2016.
+//  Copyright © 2016 Simon Bessey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
